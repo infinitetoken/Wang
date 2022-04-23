7b89360eb4d4150eb5752aa9778adc3d2ea24c12
Aaron Wright <aaron.c.wright@icloud.com>
2022-04-23 12:32:13 -0500

    Add edge generator

--- Sources/Wang/Extensions/Wang+Generator.swift
+++ Sources/Wang/Extensions/Wang+Generator.swift
@@ -11,140 +11,106 @@ import Foundation
 extension Wang {
     
     public func generate(seed: UInt8? = nil) -> [UInt8] {
-        return []
+        self.generate(seed: seed, width: self.width, height: self.height, collection: self.collection)
+    }
+
+    internal func generate(seed: UInt8?, width: UInt, height: UInt, collection: Collection) -> [UInt8] {
+        var tileIndexes: [UInt8] = []
+        
+        for row in 0..<height {
+            for column in 0..<width {
+                let index = (row * width) + column
+
+                let westValue = self.tileValue(for: index, axis: .horizontal, width: width, in: tileIndexes)
+                let northValue = self.tileValue(for: index, axis: .vertical, width: width, in: tileIndexes)
+
+                tileIndexes.append(self.generateMatchingValue(westValue: westValue, northValue: northValue, seed: seed, collection: collection))
+            }
+        }
+        
+        return tileIndexes
     }
     
-//    mutating func generateTileIndexes() {
-//        let width = Wang.previewSize.width.integerValue
-//        let height = Wang.previewSize.height.integerValue
-//
-//        switch self.style {
-//        case .blob:
-//            self.tileIndexes = self.generateBlobIndexes(width: width, height: height)
-//        case .corner:
-//            self.tileIndexes = self.generateCornerIndexes(width: width, height: height)
-//        case .edge:
-//            self.tileIndexes = self.generateEdgeIndexes(width: width, height: height)
-//        }
-//    }
-//
-//    private func generateBlobIndexes(width: Int, height: Int) -> [UInt8] {
-//        // TODO: Generate random tile indexes
-//
-//        return Array(
-//            repeating: 0,
-//            count: Wang.previewSize.width.integerValue * Wang.previewSize.height.integerValue
-//        )
-//    }
-//
-//    private func generateCornerIndexes(width: Int, height: Int) -> [UInt8] {
-//        var tileIndexes: [UInt8] = []
-//
-//        for row in 0..<Wang.previewSize.height.integerValue {
-//            for column in 0..<Wang.previewSize.width.integerValue {
-//                let index = (row * Wang.previewSize.width.integerValue) + column
-//
-//                let westValue = self.tileValue(for: index, direction: .west, in: tileIndexes)
-//                let northValue = self.tileValue(for: index, direction: .north, in: tileIndexes)
-//
-//                tileIndexes.append(self.randomCornerValue(west: westValue, north: northValue))
-//            }
-//        }
-//
-//        return tileIndexes
-//    }
-//
-//    private func generateEdgeIndexes(width: Int, height: Int) -> [UInt8] {
-//        var tileIndexes: [UInt8] = []
-//
-//        for row in 0..<Wang.previewSize.height.integerValue {
-//            for column in 0..<Wang.previewSize.width.integerValue {
-//                let index = (row * Wang.previewSize.width.integerValue) + column
-//
-//                let westValue = self.tileValue(for: index, direction: .west, in: tileIndexes)
-//                let northValue = self.tileValue(for: index, direction: .north, in: tileIndexes)
-//
-//                tileIndexes.append(self.randomEdgeValue(west: westValue, north: northValue))
-//            }
-//        }
-//
-//        return tileIndexes
-//    }
+    internal func generateMatchingValue(westValue: UInt8?, northValue: UInt8?, seed: UInt8?, collection: Collection) -> UInt8 {
+        switch collection {
+        case .blob:
+            return 0
+        case .corner:
+            return 0
+        case .edge:
+            if westValue == nil && northValue == nil { return (0..<16).randomElement() ?? 0 }
+            
+            var candidates: [UInt8] = Array(0..<16)
     
-}
-
-// MARK: - Helpers
-
-extension Wang {
+            if let westValue = westValue {
+                candidates = self.matchingValues(for: westValue, axis: .horizontal, collection: collection, in: candidates)
+            }
+            if let northValue = northValue {
+                candidates = self.matchingValues(for: northValue, axis: .vertical, collection: collection, in: candidates)
+            }
     
-//    private func tileValue(for index: Int, direction: Direction, in tileIndexes: [UInt8]) -> UInt8? {
-//        var tileIndex: Int
-//
-//        switch direction {
-//        case .north:
-//            tileIndex = index - Wang.previewSize.width.integerValue
-//        case .northeast:
-//            tileIndex = index - Wang.previewSize.width.integerValue + 1
-//        case .east:
-//            tileIndex = index + 1
-//        case .southeast:
-//            tileIndex = index + Wang.previewSize.width.integerValue + 1
-//        case .south:
-//            tileIndex = index + Wang.previewSize.width.integerValue
-//        case .southwest:
-//            tileIndex = index + Wang.previewSize.width.integerValue - 1
-//        case .west:
-//            tileIndex = index - 1
-//        case .northwest:
-//            tileIndex = index - Wang.previewSize.width.integerValue - 1
-//        }
-//
-//        if tileIndexes.indices.contains(tileIndex) {
-//            return tileIndexes[tileIndex]
-//        } else {
-//            return nil
-//        }
-//    }
-//
-//    private func randomCornerValue(west: UInt8?, north: UInt8?) -> UInt8 {
-//        if west == nil && north == nil { return 11 }//(0..<16).randomElement() ?? 0 }
-//
-//        var candidates: [UInt8] = Array(0..<16)
-//
-//        if let west = west {
-//            let northeastCandidates = self.filteredValues(style: .corner, matching: west, direction: .southeast, in: candidates)
-//            let southeastCandidates = self.filteredValues(style: .corner, matching: west, direction: .southwest, in: candidates)
-//
-//            candidates = northeastCandidates.filter(southeastCandidates.contains)
-//        }
-//        if let north = north {
-//            candidates = self.filteredValues(style: .corner, matching: north, direction: .northeast, in: candidates)
-//        }
-//
-//        return candidates.randomElement() ?? 0
-//    }
-//
-//    private func randomEdgeValue(west: UInt8?, north: UInt8?) -> UInt8 {
-//        if west == nil && north == nil { return (0..<16).randomElement() ?? 0 }
-//
-//        var candidates: [UInt8] = Array(0..<16)
-//
-//        if let west = west {
-//            candidates = self.filteredValues(style: .edge, matching: west, direction: .east, in: candidates)
-//        }
-//        if let north = north {
-//            candidates = self.filteredValues(style: .edge, matching: north, direction: .south, in: candidates)
-//        }
-//
-//        return candidates.randomElement() ?? 0
-//    }
-//
-//    private func filteredValues(style: Style, matching: UInt8, direction: Direction, in values: [UInt8]) -> [UInt8] {
-//        guard let directionMask = direction.mask(for: style) else { return [] }
-//        guard let reversedMask = direction.reversed.mask(for: style) else { return [] }
+            return candidates.randomElement() ?? 0
+        }
+    }
+    
+    internal func tileValue(for index: UInt, axis: Axis, width: UInt, in tileIndexes: [UInt8]) -> UInt8? {
+        guard index > 0 else { return nil }
+        
+        switch axis {
+        case .horizontal:
+            return tileIndexes.indices.contains(Int(index) - 1) ? tileIndexes[Int(index) - 1] : nil
+        case .vertical:
+            return tileIndexes.indices.contains(Int(index) - Int(width)) ? tileIndexes[Int(index) - Int(width)] : nil
+        }
+    }
+    
+    internal func matchingValues(for value: UInt8, axis: Axis, collection: Collection, in values: [UInt8]) -> [UInt8] {
+        switch axis {
+        case .horizontal:
+            switch collection {
+            case .blob:
+                return []
+            case .corner:
+                return []
+            case .edge:
+                guard let eastMask = Cardinal.east.mask(for: .edge) else { return [] }
+                guard let westMask = Cardinal.west.mask(for: .edge) else { return [] }
+                
+                return values.filter { value in
+                    if (value & eastMask) == eastMask {
+                        return (value & westMask) != westMask
+                    } else {
+                        return (value & westMask) == westMask
+                    }
+                }
+            }
+        case .vertical:
+            switch collection {
+            case .blob:
+                return []
+            case .corner:
+                return []
+            case .edge:
+                guard let southMask = Cardinal.south.mask(for: .edge) else { return [] }
+                guard let northMask = Cardinal.north.mask(for: .edge) else { return [] }
+                
+                return values.filter { value in
+                    if (value & southMask) == southMask {
+                        return (value & northMask) != northMask
+                    } else {
+                        return (value & northMask) == northMask
+                    }
+                }
+            }
+        }
+    }
+    
+//    internal func filteredValues(collection: Collection, matchingValue: UInt8, cardinal: Cardinal, in values: [UInt8]) -> [UInt8] {
+//        guard let directionMask = cardinal.mask(for: style) else { return [] }
+//        guard let reversedMask = cardinal.reversed.mask(for: style) else { return [] }
 //
 //        let candidates = values.filter { value in
-//            if (matching & directionMask) == directionMask {
+//            if (matchingValue & directionMask) == directionMask {
 //                return (value & reversedMask) != reversedMask
 //            } else {
 //                return (value & reversedMask) == reversedMask
@@ -153,5 +119,7 @@ extension Wang {
 //
 //        return candidates
 //    }
+
+//
     
 }
